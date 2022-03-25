811f6fe87bb4d03e6756118953a764ad131a389a
Mat Talma <mattalma11@gmail.com>
2022-03-25 18:05:23 -0400

    End of Section 19

--- API/Startup.cs
+++ API/Startup.cs
@@ -4,6 +4,7 @@ using FluentValidation.AspNetCore;
 using API.Middleware;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Authorization;
+using API.SignalR;
 
 namespace API
 {
@@ -54,6 +55,7 @@ namespace API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHub<ChatHub>("/chat");
             });
         }
     }
